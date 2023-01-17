080c6912cddcf4d46c3d1757af70d4db51fc113b
Saul <claudio.velecela.est@tecazuay.edu.ec>
2023-01-17 09:11:33 -0500

    eliminado

--- src/main/java/com/ista/springboot/web/swagger/models/dao/IEmpleadoDao.java
+++
@@ -1,9 +0,0 @@
-package com.ista.springboot.web.swagger.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ista.springboot.web.swagger.apirest.models.entity.Empleado;
-
-public interface IEmpleadoDao extends CrudRepository<Empleado, Long>{
-
-}
--- src/main/java/com/ista/springboot/web/swagger/models/dao/IProductoDao.java
+++
@@ -1,9 +0,0 @@
-package com.ista.springboot.web.swagger.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ista.springboot.web.swagger.apirest.models.entity.Producto;
-
-public interface IProductoDao extends CrudRepository<Producto, Long>{
-
-}
